084d00709dc87036d3e16071a6a31b5dd7a78013
Felipe Castillo <1006071586@u.icesi.edu.co>
2025-03-17 18:41:25 -0500

    feat: integrate RabbitMQ with trainers-management-service

--- Trainers-Management-Service/icesi/src/main/java/co/edu/icesi/service/EntrenadorService.java
+++ Trainers-Management-Service/icesi/src/main/java/co/edu/icesi/service/EntrenadorService.java
@@ -2,6 +2,9 @@ package co.edu.icesi.service;
 
 import co.edu.icesi.model.Entrenador;
 import co.edu.icesi.repository.EntrenadorRepository;
+import co.edu.icesi.dto.NotificacionDTO;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,6 +14,9 @@ public class EntrenadorService {
 
     private final EntrenadorRepository entrenadorRepository;
 
+    @Autowired
+    private RabbitTemplate rabbitTemplate; // Para enviar notificaciones
+
     public EntrenadorService(EntrenadorRepository entrenadorRepository) {
         this.entrenadorRepository = entrenadorRepository;
     }
@@ -18,8 +24,22 @@ public class EntrenadorService {
     public List<Entrenador> listarEntrenadores() {
         return entrenadorRepository.findAll();
     }
+
     public Entrenador agregarEntrenador(Entrenador entrenador) {
-        return entrenadorRepository.save(entrenador);
+        Entrenador nuevoEntrenador = entrenadorRepository.save(entrenador);
+
+        // Enviar notificación (ejemplo)
+        NotificacionDTO notificacion = new NotificacionDTO(
+                "admin",
+                "Se ha agregado el entrenador: " + (nuevoEntrenador.getNombre() != null ? nuevoEntrenador.getNombre() : "")
+        );
+        rabbitTemplate.convertAndSend(
+                "notificacion.exchange",
+                "notificacion.routingkey",
+                notificacion
+        );
+
+        return nuevoEntrenador;
     }
 
     public Entrenador obtenerEntrenador(Long id) {
